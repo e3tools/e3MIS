19bc72cbab8905dd9a682e8fc1b5e7e2bbeb3d84
AndreaSucreG <andresucre88@gmail.com>
2025-04-14 22:59:11 +0200

    Subproject list

--- subprojects/urls.py
+++ subprojects/urls.py
@@ -1,8 +1,13 @@
+from django.urls import path
+
+from subprojects.infrastructure.views.subproject_create_view import SubprojectCreateView
+from subprojects.infrastructure.views.subproject_list_view import SubprojectListView
 
 app_name = 'subprojects'
 
 urlpatterns = [
-
+    path('', SubprojectListView.as_view(), name='subproject_list'),
+    path('/create', SubprojectCreateView.as_view(), name='subproject_create'),
 
 ]
 
