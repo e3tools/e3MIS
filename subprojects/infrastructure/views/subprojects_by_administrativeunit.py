70c6982b590aba03fbfea7da21a984da5df6c221
NestorBracho <nestorbracho2207@gmail.com>
2025-05-21 22:25:29 +0200

    dynamic selects

--- subprojects/infrastructure/views/subprojects_by_administrativeunit.py
+++ subprojects/infrastructure/views/subprojects_by_administrativeunit.py
@@ -27,6 +27,8 @@ class AdministrativeUnitSubprojectsView(LoginRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['selects'] = self.get_admnistrative_unit_selects_structure()
+        context['initial_select_values'] = self.request.user.administrative_unit.children.all()
         context['title'] = f"Subprojects of {self.request.user.administrative_unit.name}" if self.request.user.administrative_unit else "No Assigned Unit"
         return context
 
@@ -39,3 +41,13 @@ class AdministrativeUnitSubprojectsView(LoginRequiredMixin, ListView):
         self.id_list = list()
         _get_children_ids(administrative_unit)
         return self.id_list
+
+    def get_admnistrative_unit_selects_structure(self):
+        def _get_administrative_unit_selects_structure(au, au_list):
+            au_list.append({'name': au.level.name, 'id': au.level.id, 'order': au.level.order})
+            if au.children.exists():
+                au_list = _get_administrative_unit_selects_structure(au.children.first(), au_list)
+            return au_list
+        select_list = list()
+        user_unit = self.request.user.administrative_unit
+        return _get_administrative_unit_selects_structure(user_unit, select_list)
