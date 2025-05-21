5b435c29bdeee1ff15687a44ec6351515003ac0b
NestorBracho <nestorbracho2207@gmail.com>
2025-05-21 18:25:25 +0200

    administrative_unit_subprojects

--- subprojects/infrastructure/views/subprojects_by_administrativeunit.py
+++ subprojects/infrastructure/views/subprojects_by_administrativeunit.py
@@ -1,21 +1,27 @@
 from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from administrativelevels.models import AdministrativeUnit
+from subprojects.models import Subproject
 
 
-class AdministrativeUnitChildrenView(LoginRequiredMixin, ListView):
-    model = AdministrativeUnit
+class AdministrativeUnitSubprojectsView(LoginRequiredMixin, ListView):
+    model = Subproject
     template_name = 'administrative_levels/administrative_unit_children.html'
-    context_object_name = 'children'
+    context_object_name = 'subprojects'
     login_url = '/login/'
 
     def get_queryset(self):
         user_unit = self.request.user.administrative_unit
-        if user_unit:
-            return AdministrativeUnit.objects.filter(parent=user_unit)
-        return AdministrativeUnit.objects.none()
+        queryset = self.model._default_manager.filter(administrative_level=user_unit)
+
+        ordering = self.get_ordering()
+        if ordering:
+            if isinstance(ordering, str):
+                ordering = (ordering,)
+            queryset = queryset.order_by(*ordering)
+
+        return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = f"Children of {self.request.user.administrative_unit.name}" if self.request.user.administrative_unit else "No Assigned Unit"
+        context['title'] = f"Subprojects of {self.request.user.administrative_unit.name}" if self.request.user.administrative_unit else "No Assigned Unit"
         return context
