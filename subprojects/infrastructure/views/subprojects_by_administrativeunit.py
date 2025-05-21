8f348d5ae73eb4f4600457bfd6952870cf7a8d03
NestorBracho <nestorbracho2207@gmail.com>
2025-05-21 18:54:23 +0200

    recursive administrative unit filter

--- subprojects/infrastructure/views/subprojects_by_administrativeunit.py
+++ subprojects/infrastructure/views/subprojects_by_administrativeunit.py
@@ -8,10 +8,14 @@ class AdministrativeUnitSubprojectsView(LoginRequiredMixin, ListView):
     template_name = 'administrative_levels/administrative_unit_children.html'
     context_object_name = 'subprojects'
     login_url = '/login/'
+    id_list = None
 
     def get_queryset(self):
         user_unit = self.request.user.administrative_unit
-        queryset = self.model._default_manager.filter(administrative_level=user_unit)
+
+        queryset = self.model._default_manager.filter(
+            administrative_level__id__in=self.get_children_ids(user_unit)
+        )
 
         ordering = self.get_ordering()
         if ordering:
@@ -25,3 +29,13 @@ class AdministrativeUnitSubprojectsView(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context['title'] = f"Subprojects of {self.request.user.administrative_unit.name}" if self.request.user.administrative_unit else "No Assigned Unit"
         return context
+
+    def get_children_ids(self, administrative_unit):
+        def _get_children_ids(au):
+            for child in au.children.all():
+                _get_children_ids(child)
+            self.id_list.append(au.id)
+
+        self.id_list = list()
+        _get_children_ids(administrative_unit)
+        return self.id_list
