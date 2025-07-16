260f33420e640c2fdd5c37617d1270c951b204f0
NestorBracho <nestorbracho2207@gmail.com>
2025-07-16 17:28:51 +0200

    success message creat custom form

--- subprojects/infrastructure/views/subproject_custom_form_create_view.py
+++ subprojects/infrastructure/views/subproject_custom_form_create_view.py
@@ -1,6 +1,8 @@
 # subprojects/views.py
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
+from django.contrib import messages
+from django.utils.translation import gettext as _
 from subprojects.models import SubprojectCustomField
 from subprojects.infrastructure.forms.subproject_custom_form import SubprojectCustomFieldsForm
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -17,17 +19,7 @@ class SubprojectCustomFieldsCreateView(LoginRequiredMixin, IsStaffMemberMixin, C
         kwargs.update({'custom_forms': self.model.objects.all()})
         return super().get_context_data(**kwargs)
 
-    def post(self, request, *args, **kwargs):
-        print(request.POST)
-        form = self.get_form()
-        if form.is_valid():
-            print('here now')
-            return self.form_valid(form)
-        else:
-            print('invalid form')
-            return self.form_invalid(form)
-
     def form_valid(self, form):
-        print("SubprojectCustomFieldsCreateView.form_valid")
-        # Any extra processing here
-        return super().form_valid(form)
+        response = super().form_valid(form)
+        messages.success(self.request, _('Your Custom Form was successfully created or updated.'))
+        return response
