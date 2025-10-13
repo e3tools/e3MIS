ca23312a57dcc864dd0c223be5c0da7340e7bcc2
NestorBracho <nestorbracho2207@gmail.com>
2025-10-13 15:54:18 +0200

    Dependencies in Follow up Events & Dependencies across fields in same form

--- trackableobjects/infrastructure/mobile_views/select_follow_up_event.py
+++ trackableobjects/infrastructure/mobile_views/select_follow_up_event.py
@@ -1,17 +1,49 @@
 from django.views.generic import TemplateView
-from django.db.models import Q, OuterRef, Count, Subquery, IntegerField, F
+from django.db.models import Q, OuterRef, Count, Subquery, IntegerField, F, Exists
 from src.permissions import IsFieldAgentUserMixin
-from trackableobjects.models import FollowUpEvent, FollowUpEventResponse, TrackableObjectInstance
+from trackableobjects.models import (
+    FollowUpEvent,
+    FollowUpEventResponse,
+    TrackableObjectInstance,
+    FollowUpEventDependency
+)
 
 
 class SelectFollowUpEventView(IsFieldAgentUserMixin, TemplateView):
     template_name = 'trackable_objects/mobile/select_follow_up_event.html'
 
     def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         user_group_ids = list(self.request.user.groups.values_list('id', flat=True))
-        kwargs['trackable_object_instance'] = TrackableObjectInstance.objects.filter(id=self.kwargs['pk']).select_related(
-            'trackable_object').first()
 
+        trackable_object_instance = TrackableObjectInstance.objects.select_related(
+            'trackable_object'
+        ).get(id=self.kwargs['pk'])
+
+        context['trackable_object_instance'] = trackable_object_instance
+
+        # Get base queryset with all necessary annotations
+        follow_up_events_qs = self._get_follow_up_events_queryset(
+            user_group_ids,
+            trackable_object_instance
+        )
+
+        # Split into one-off and repeating events
+        context['one_off_follow_up_events'] = follow_up_events_qs.filter(is_one_off=True)
+        context['repeating_follow_up_events'] = follow_up_events_qs.filter(is_one_off=False)
+
+        # Annotate one-off events with response status
+        self._annotate_response_status(
+            context['one_off_follow_up_events'],
+            trackable_object_instance
+        )
+
+        return context
+
+    def _get_follow_up_events_queryset(self, user_group_ids, trackable_object_instance):
+        """Returns queryset of FollowUpEvents filtered by permissions and dependencies."""
+
+        # Subquery: Count matching user groups
         matching_group_count = FollowUpEvent.groups.through.objects.filter(
             followupevent_id=OuterRef('pk'),
             group_id__in=user_group_ids
@@ -19,42 +51,38 @@ class SelectFollowUpEventView(IsFieldAgentUserMixin, TemplateView):
             count=Count('group_id')
         ).values('count')
 
-        one_off_follow_up_events = FollowUpEvent.objects.annotate(
-                total_groups=Count('groups', distinct=True),
-                matched_groups=Subquery(matching_group_count, output_field=IntegerField()),
-            ).filter(
-                Q(total_groups=0) |
-                Q(total_groups=F('matched_groups'))
-            ).filter(
-                trackable_object=kwargs['trackable_object_instance'].trackable_object,
-                is_one_off=True,
-                is_active=True
-            ).distinct()
-
-        repeating_follow_up_events = FollowUpEvent.objects.annotate(
-                total_groups=Count('groups', distinct=True),
-                matched_groups=Subquery(matching_group_count, output_field=IntegerField()),
-            ).filter(
-                Q(total_groups=0) |
-                Q(total_groups=F('matched_groups'))
-            ).filter(
-                trackable_object=kwargs['trackable_object_instance'].trackable_object,
-                is_one_off=False,
-                is_active=True
-            ).distinct()
-
-        kwargs.update({'one_off_follow_up_events': one_off_follow_up_events})
-        kwargs.update({'repeating_follow_up_events': repeating_follow_up_events})
-
-        for follow_up_event in kwargs['one_off_follow_up_events']:
-            qs = FollowUpEventResponse.objects.filter(
-                    follow_up_event=follow_up_event,
-                    trackable_object_instance=kwargs['trackable_object_instance']
+        # Subquery: Check for unfulfilled parent dependencies
+        unfulfilled_dependencies = FollowUpEventDependency.objects.filter(
+            child_id=OuterRef('pk')
+        ).exclude(
+            parent_id__in=Subquery(
+                FollowUpEventResponse.objects.filter(
+                    trackable_object_instance=trackable_object_instance
+                ).values('follow_up_event_id')
             )
-            if not qs.exists():
-                follow_up_event.has_no_response = True
-            else:
-                follow_up_event.has_no_response = False
-                follow_up_event.response_id = qs.first().id
+        )
 
-        return super().get_context_data(**kwargs)
+        return FollowUpEvent.objects.annotate(
+            total_groups=Count('groups', distinct=True),
+            matched_groups=Subquery(matching_group_count, output_field=IntegerField()),
+            has_unfulfilled_deps=Exists(unfulfilled_dependencies)
+        ).filter(
+            # User has permission (either no groups or user is in all required groups)
+            Q(total_groups=0) | Q(total_groups=F('matched_groups')),
+            # Event applies to this trackable object
+            Q(trackable_objects=trackable_object_instance.trackable_object) | Q(trackable_objects=None),
+            # Event is active and has no unfulfilled dependencies
+            is_active=True,
+            has_unfulfilled_deps=False
+        ).distinct()
+
+    def _annotate_response_status(self, follow_up_events, trackable_object_instance):
+        """Adds has_no_response and response_id attributes to one-off events."""
+        for follow_up_event in follow_up_events:
+            response = FollowUpEventResponse.objects.filter(
+                follow_up_event=follow_up_event,
+                trackable_object_instance=trackable_object_instance
+            ).first()
+
+            follow_up_event.has_no_response = response is None
+            follow_up_event.response_id = response.id if response else None
