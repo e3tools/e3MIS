4a511b94437f20525e41d536f5b79fd36e1a9988
NestorBracho <nestorbracho2207@gmail.com>
2026-01-19 17:03:58 +0100

    administrative unit fix

--- administrativelevels/forms.py
+++ administrativelevels/forms.py
@@ -13,31 +13,13 @@ class AdministrativeLevelForm(forms.ModelForm):
 
 
 class AdministrativeUnitForm(forms.ModelForm):
+    """Simple form for adding units - only requires name"""
     class Meta:
         model = AdministrativeUnit
-        fields = ['name', 'level', 'parent']
+        fields = ['name']
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control'}),
-            'level': forms.Select(attrs={'class': 'form-control'}),
-            'parent': forms.Select(attrs={'class': 'form-control'}),
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Unit name'}),
         }
-
-    def __init__(self, *args, **kwargs):
-        parent_unit = kwargs.pop('parent_unit', None)
-        super().__init__(*args, **kwargs)
-
-        if parent_unit:
-            # When adding a child, set parent and filter levels to show only the next level
-            self.fields['parent'].initial = parent_unit
-            self.fields['parent'].widget = forms.HiddenInput()
-
-            # Get the next level in hierarchy
-            parent_level_order = parent_unit.level.order
-            next_levels = AdministrativeLevel.objects.filter(order=parent_level_order + 1)
-            self.fields['level'].queryset = next_levels
-
-            if next_levels.count() == 1:
-                self.fields['level'].initial = next_levels.first()
 
 
 class AdministrativeUnitEditForm(forms.ModelForm):
