e8bc5f65da5c94a39f7249884db7d1a7a9aee8f5
Leonardo Gutierrez <leonardogutierrezh@gmail.com>
2026-02-26 17:21:51 +0100

    Enhances UI navigation and consistency
    Introduces a standardized custom title block across various `trackable_objects` templates. This block includes a clear page title and a back button for improved user navigation.
    
    Updates several views to provide appropriate page titles via `extra_context` or `get_context_data`.
    
    Refines navigation links in mobile templates, replacing generic history navigation with explicit URL paths for more predictable back functionality and ensuring 'home' buttons link to the module's main index.

--- trackableobjects/infrastructure/views/follow_up_event_list_view.py
+++ trackableobjects/infrastructure/views/follow_up_event_list_view.py
@@ -1,10 +1,14 @@
 from django.views.generic import ListView
+from django.utils.translation import gettext_lazy as _
 from trackableobjects.models import FollowUpEvent, TrackableObject
 
 
 class FollowUpEventListView(ListView):
     template_name = 'trackable_objects/follow_up_event_list.html'
     model = FollowUpEvent
+    extra_context = {
+        'title': _('Follow Up Events'),
+    }
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(object_list=object_list, **kwargs)
