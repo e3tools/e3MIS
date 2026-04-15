9a44a5e431c8d257b09ecbf814bc248f594b2e3d
Florentin <mawuetin-jean-florentin.aboe@epitech.eu>
2026-04-15 10:04:14 +0100

    delete register in api/routers for missing ViewSet

--- trackableobjects/api/routers.py
+++ trackableobjects/api/routers.py
@@ -4,8 +4,8 @@ from trackableobjects.api import views
 
 router = routers.SimpleRouter()
 
-router.register(r'trackable-object', views.TrackableObjectModelViewSet)
-router.register(r'trackable-object-instance', views.TrackableObjectInstanceModelViewSet)
-router.register(r'follow-up-event', views.FollowUpEventModelViewSet)
-router.register(r'follow-up-event-response', views.FollowUpEventResponseModelViewSet)
-router.register(r'follow-up-event-dependency', views.FollowUpEventDependencyModelViewSet)
+# router.register(r'trackable-object', views.TrackableObjectModelViewSet)
+# router.register(r'trackable-object-instance', views.TrackableObjectInstanceModelViewSet)
+# router.register(r'follow-up-event', views.FollowUpEventModelViewSet)
+# router.register(r'follow-up-event-response', views.FollowUpEventResponseModelViewSet)
+# router.register(r'follow-up-event-dependency', views.FollowUpEventDependencyModelViewSet)
